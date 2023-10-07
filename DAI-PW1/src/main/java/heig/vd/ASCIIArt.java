44fb4bcaa23fc0967cfb9d5b0ed4459fe73dbf34
sarah jallon <sarah.jallon@gmail.com>
2023-10-07 20:00:42 +0200

    [feat] we can read an input from a file and print it in the console

--- DAI-PW1/src/main/java/heig/vd/ASCIIArt.java
+++ DAI-PW1/src/main/java/heig/vd/ASCIIArt.java
@@ -14,18 +14,16 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 @Command(name = "ASCIIArt", version = "ASCIIArt 1.0", mixinStandardHelpOptions = true) // |1|
 public class ASCIIArt implements Runnable { // |2|
 
     @Option(names = { "-s", "--font-size" }, description = "Font size") // |3|
     int fontSize = 14;
-
-    @Parameters(paramLabel = "<word>",  // |4|
-            description = "Words to be translated into ASCII art.")
-    private String word;
-
     @Parameters(paramLabel = "<inputFile", defaultValue = "./inputFile.txt",
                 description = "File to be read as an input.")
     private File inputFile;
@@ -34,39 +32,64 @@ public class ASCIIArt implements Runnable { // |2|
             description = "File where the result will be written.")
     private File outputFile;
 
+
     @Override
     public void run() { // |6|
-        System.out.println( word);
-        System.out.println(inputFile.getPath());
-        System.out.println(outputFile.getPath());
-        //source: https://www.baeldung.com/ascii-art-in-java
-        //create a buffered image using integer mode as image type
-      /*  int width = 144, height = 32;
-        BufferedImage bufferedImage = new BufferedImage(
-                width, height,
-                BufferedImage.TYPE_INT_RGB);
+        //get the text from the input file
+        try {
+            // Specify the path to your text file
 
-        Graphics graphics = bufferedImage.getGraphics();
-        graphics.setFont(new Font("Dialog", Font.PLAIN, fontSize));
-        Graphics2D graphics2D = (Graphics2D) graphics;
-        graphics2D.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+            // Create a StringBuilder to store the file content
+            StringBuilder fileContent = new StringBuilder();
 
-        graphics2D.drawString(String.join(" ", words), 6, 24);
+            // Open the file for reading using FileReader and BufferedReader
+            FileReader fileReader = new FileReader(inputFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        for (int y = 0; y < height; y++) {
-            StringBuilder stringBuilder = new StringBuilder();
-
-            for (int x = 0; x < width; x++) {
-                stringBuilder.append(bufferedImage.getRGB(x, y) == -16777216 ? " " : "*");
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                // Append each line to the StringBuilder with a newline separator
+                fileContent.append(line).append("\n");
             }
 
-            if (stringBuilder.toString().trim().isEmpty()) {
-                continue;
-            }
+            // Close the BufferedReader and FileReader
+            bufferedReader.close();
+            fileReader.close();
 
-            System.out.println(stringBuilder);
-        }*/
+            // Convert the StringBuilder to a String
+            String fileContentAsString = fileContent.toString();
+
+            //source: https://www.baeldung.com/ascii-art-in-java
+
+            //create a buffered image using integer mode as image type
+            int width = 144, height = 32;
+            BufferedImage bufferedImage = new BufferedImage(
+                    width, height,
+                    BufferedImage.TYPE_INT_RGB);
+
+            //
+            Graphics graphics = bufferedImage.getGraphics();
+            Graphics2D graphics2D = (Graphics2D) graphics;
+            graphics2D.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                    RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+            graphics2D.drawString(fileContentAsString, 12, 24);
+
+            for (int y = 0; y < height; y++) {
+                StringBuilder stringBuilder = new StringBuilder();
+
+                for (int x = 0; x < width; x++) {
+                    stringBuilder.append(bufferedImage.getRGB(x, y) == -16777216 ? " " : "*");
+                }
+
+                if (stringBuilder.toString().trim().isEmpty()) {
+                    continue;
+                }
+
+                System.out.println(stringBuilder);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
